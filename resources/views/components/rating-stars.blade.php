a35626c9ea6d81f73383547c30bd2ec62d406e17
Ziad Abdelhamid <ziadzizo1999@gmail.com>
2023-04-26 17:29:22 +0300

    Reformatting

--- resources/views/components/rating-stars.blade.php
+++ resources/views/components/rating-stars.blade.php
@@ -1,16 +1,15 @@
 @php
-$rating = $movie->rating;
-for ($i=0; $i < 5; $i++) { if ($rating> 0){
-    if ($rating > 0.5){
-    echo '<i class="fa fa-star"></i>';
+    $rating = $movie->rating;
+    for ($i = 0; $i < 5; $i++) {
+        if ($rating > 0) {
+            if ($rating > 0.5) {
+                echo '<i class="fa fa-star"></i>';
+            } else {
+                echo '<i class="fa fa-star-half-o"></i>';
+            }
+        } else {
+            echo '<i class="fa fa-star-o"></i>';
+        }
+        $rating--;
     }
-    else {
-    echo '<i class="fa fa-star-half-o"></i>';
-    }
-    }
-    else {
-    echo '<i class="fa fa-star-o"></i>';
-    }
-    $rating--;
-    }
-    @endphp
+@endphp
