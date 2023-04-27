d025631a83eb57ba4a0d73be3bfeac1997cefaee
Ziad Abdelhamid <ziadzizo1999@gmail.com>
2023-04-28 02:19:04 +0300

    Updated footer links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -11,10 +11,12 @@
                     <div class="widget widget-about">
 
                         <!-- INSERT YOUR LOGO HERE -->
-                        <img src={{  asset('images/branding/logos/logo-bt.png')  }} alt="logo" class="logo">
+                        <img src={{ asset('images/branding/logos/logo-bt.png') }} alt="logo" class="logo">
                         <!-- INSERT YOUR WHITE LOGO HERE -->
-                        <img src={{  asset('images/branding/logos/logo-w.png')  }} alt="white logo" class="logo-white">
-                        <p class="nomargin">{{  config('app.name')  }} is a ticket reservation system designed for cinemas. It features a great number of features, from normal ticket reservation to multi-user roles. It was developed using laravel 8. We hope you like it.</p>
+                        <img src={{ asset('images/branding/logos/logo-w.png') }} alt="white logo" class="logo-white">
+                        <p class="nomargin">{{ config('app.name') }} is a ticket reservation system designed for
+                            cinemas. It features a great number of features, from normal ticket reservation to
+                            multi-user roles. It was developed using laravel 8. We hope you like it.</p>
                     </div>
                 </div>
                 <!-- End of Widget 1 -->
@@ -22,14 +24,13 @@
                 <!-- Start of Widget 2 -->
                 <div class="col-lg-3 col-md-6 col-sm-12">
                     <div class="widget widget-links">
-                        <h4 class="widget-title">Sitemap</h4>
+                        <h4 class="widget-title">Useful Links</h4>
 
                         <ul class="general-listing">
-                            <li><a href="#">about movify</a></li>
-                            <li><a href="#">blog</a></li>
-                            <li><a href="#">forum</a></li>
-                            <li><a href="#">my account</a></li>
-                            <li><a href="#">watch list</a></li>
+                            <li><a href="{{ route('home') }}">Home</a></li>
+                            <li><a href="{{ route('movies.index') }}">Movies</a></li>
+                            <li><a href="{{ route('movies.index') }}">What to watch</a></li>
+                            <li><a href="{{ route('dashboard') }}">my account</a></li>
                         </ul>
 
                     </div>
@@ -39,12 +40,19 @@
                 <!-- Start of Widget 3 -->
                 <div class="col-lg-3 col-md-6 col-sm-12">
                     <div class="widget widget-blog">
-                        <h4 class="widget-title">Nearest Movies</h4>
+                        @php
+                            $newestMovies = \App\Models\Movie::orderBy('release_date', 'desc')
+                                ->take(3)
+                                ->get();
+                        @endphp
+                        <h4 class="widget-title">Newest Movies</h4>
 
                         <ul class="blog-posts">
-                            <li><a href="#">blog post 1</a><small>januar 13, 2018</small></li>
-                            <li><a href="#">blog post 2</a><small>januar 13, 2018</small></li>
-                            <li><a href="#">blog post 3</a><small>januar 13, 2018</small></li>
+                            @foreach ($newestMovies as $movie)
+                                <li><a
+                                        href="#">{{ $movie->title }}</a><small>{{ $movie->release_date->format('d M Y') }}</small>
+                                </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
