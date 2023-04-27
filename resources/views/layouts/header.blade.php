a9cab00502041ff9d69fa3ba7abeb41835631246
Ziad Abdelhamid <ziadzizo1999@gmail.com>
2023-04-28 01:14:13 +0300

    Added link to movies page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,8 @@
                 <!-- INSERT YOUR LOGO HERE -->
                 <img src="{{ asset('images/branding/logos/logo-bt.png') }}" alt="logo" width="200" class="logo">
                 <!-- INSERT YOUR WHITE LOGO HERE -->
-                <img src="{{ asset('images/branding/logos/logo-w.png') }}" alt="white logo" width="200" class="logo-white">
+                <img src="{{ asset('images/branding/logos/logo-w.png') }}" alt="white logo" width="200"
+                    class="logo-white">
             </a>
 
             <!-- Login Button on Responsive -->
@@ -33,17 +34,17 @@
                 <ul class="navbar-nav mx-auto" id="main-menu">
                     <!-- Menu Item -->
                     <li class="nav-item">
-                        <a class="nav-link" href={{  route('home')  }}>Home</a>
+                        <a class="nav-link" href={{ route('home') }}>Home</a>
                     </li>
 
                     <!-- Menu Item -->
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Movies</a>
+                        <a class="nav-link" href="{{ route('movies.index') }}">Movies</a>
                     </li>
 
                     <!-- Menu Item -->
                     <li class="nav-item">
-                        <a class="nav-link" href="#">What to Watch?</a>
+                        <a class="nav-link" href="{{ route('movies.index') }}">What to Watch?</a>
                     </li>
 
                     <!-- Menu Item -->
@@ -60,38 +61,39 @@
 
                     <!-- Menu Item -->
                     @auth
-                    <li class="nav-item notification-wrapper">
-                        <a class="nav-link notification" href="{{ route('dashboard') }}">
-                            <i class="fa fa-ticket"></i>
-                            <span class="notification-count">{{ auth()->user()->reservations->count() }}</span>
-                        </a>
-                    </li>
+                        <li class="nav-item notification-wrapper">
+                            <a class="nav-link notification" href="{{ route('dashboard') }}">
+                                <i class="fa fa-ticket"></i>
+                                <span class="notification-count">{{ auth()->user()->reservations->count() }}</span>
+                            </a>
+                        </li>
                     @endauth
 
                     @auth
-                    <li class="nav-item dropdown" style="z-index: 50;">
-                        <a href="{{ route('dashboard') }}" class="btn btn-main btn-effect login-btn">
-                            <i class="icon-user"></i>Hello, {{ auth()->user()->first_name }}</a>
-                        <div class="dropdown-content rounded font-weight-normal">
-                            <a href="{{ route('dashboard') }}">My Account</a>
-                            <a href="{{ route('dashboard') }}">My Reservations</a>
-                            @can('admin')
-                            <a href="{{ route('admin.dashboard') }}">Admin Dashboard</a>
-                            @endcan
-                            @can('manager')
-                            <a href="{{ route('manager.dashboard') }}">Manager Dashboard</a>
-                            @endcan
-                            <form id="logout_form" method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <a href="javascript:{}" onclick="document.getElementById('logout_form').submit();">Log Out</a>
-                            </form>
-                        </div>
-                    </li>
+                        <li class="nav-item dropdown" style="z-index: 50;">
+                            <a href="{{ route('dashboard') }}" class="btn btn-main btn-effect login-btn">
+                                <i class="icon-user"></i>Hello, {{ auth()->user()->first_name }}</a>
+                            <div class="dropdown-content rounded font-weight-normal">
+                                <a href="{{ route('dashboard') }}">My Account</a>
+                                <a href="{{ route('dashboard') }}">My Reservations</a>
+                                @can('admin')
+                                    <a href="{{ route('admin.dashboard') }}">Admin Dashboard</a>
+                                @endcan
+                                @can('manager')
+                                    <a href="{{ route('manager.dashboard') }}">Manager Dashboard</a>
+                                @endcan
+                                <form id="logout_form" method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <a href="javascript:{}" onclick="document.getElementById('logout_form').submit();">Log
+                                        Out</a>
+                                </form>
+                            </div>
+                        </li>
                     @else
-                    <li class="nav-item">
-                        <a href="{{  route('login')  }}" class="btn btn-main btn-effect login-btn">
-                            <i class="icon-user"></i>login</a>
-                    </li>
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="btn btn-main btn-effect login-btn">
+                                <i class="icon-user"></i>login</a>
+                        </li>
                     @endauth
 
                 </ul>
