de2281120f3b5f50720e8cd852e300aebb4ed2b4
Ziad Abdelhamid <ziadzizo1999@gmail.com>
2023-04-28 04:28:24 +0300

    minified admin css assets

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -2,8 +2,7 @@
 <html lang="en">
 
 @php
-$requests = \App\Models\User::where('wants_manager',1)->get();
-// ddd($requests)
+    $requests = \App\Models\User::where('wants_manager', 1)->get();
 @endphp
 
 <head>
@@ -24,7 +23,9 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
     <link rel="stylesheet" href="{{ asset('css/nunito-font.css') }}">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('css/sb-admin-2.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+    @stack('head')
 
 </head>
 
@@ -45,7 +46,7 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item {{ \Request::route()->getName() === 'admin.dashboard'?'active':'' }}">
+            <li class="nav-item {{ \Request::route()->getName() === 'admin.dashboard' ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('admin.dashboard') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
@@ -57,13 +58,13 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
             <!-- Heading -->
             <div class="sidebar-heading">Users</div>
 
-            <li class="nav-item {{ \Request::route()->getName() === 'users.index'?'active':'' }}">
+            <li class="nav-item {{ \Request::route()->getName() === 'users.index' ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('users.index') }}">
                     <i class="fas fa-fw fa-users"></i>
                     <span>View Users</span></a>
             </li>
 
-            <li class="nav-item {{ \Request::route()->getName() === 'users.manager-requests'?'active':'' }}">
+            <li class="nav-item {{ \Request::route()->getName() === 'users.manager-requests' ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('users.manager-requests') }}">
                     <i class="fas fa-fw fa-user-tie"></i>
                     <span>Manager Requests</span></a>
@@ -94,9 +95,11 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
                     </button>
 
                     <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                    <form
+                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                         <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                            <input type="text" class="form-control bg-light border-0 small"
+                                placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append">
                                 <button class="btn btn-primary" type="button">
                                     <i class="fas fa-search fa-sm"></i>
@@ -110,14 +113,18 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
                         <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-search fa-fw"></i>
                             </a>
                             <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                                aria-labelledby="searchDropdown">
                                 <form class="form-inline mr-auto w-100 navbar-search">
                                     <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                        <input type="text" class="form-control bg-light border-0 small"
+                                            placeholder="Search for..." aria-label="Search"
+                                            aria-describedby="basic-addon2">
                                         <div class="input-group-append">
                                             <button class="btn btn-primary" type="button">
                                                 <i class="fas fa-search fa-sm"></i>
@@ -130,30 +137,36 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
 
                         <!-- Nav Item - Alerts -->
                         <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-bell fa-fw"></i>
                                 <!-- Counter - Alerts -->
                                 <span class="badge badge-danger badge-counter">{{ $requests->count() }}</span>
                             </a>
                             <!-- Dropdown - Alerts -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="alertsDropdown">
                                 <h6 class="dropdown-header">
                                     Alerts Center
                                 </h6>
                                 @foreach ($requests->take(5) as $request)
-                                <a class="dropdown-item d-flex align-items-center" href="{{ route('users.manager-requests').'#'.$request->id }}">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-primary">
-                                            <i class="fas fa-user text-white"></i>
+                                    <a class="dropdown-item d-flex align-items-center"
+                                        href="{{ route('users.manager-requests') . '#' . $request->id }}">
+                                        <div class="mr-3">
+                                            <div class="icon-circle bg-primary">
+                                                <i class="fas fa-user text-white"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">{{ $request->created_at->diffForHumans() }}</div>
-                                        <span class="font-weight-bold">{{ $request->username }} has requested to become a manager.</span>
-                                    </div>
-                                </a>
+                                        <div>
+                                            <div class="small text-gray-500">
+                                                {{ $request->created_at->diffForHumans() }}</div>
+                                            <span class="font-weight-bold">{{ $request->username }} has requested to
+                                                become a manager.</span>
+                                        </div>
+                                    </a>
                                 @endforeach
-                                <a class="dropdown-item text-center small text-gray-500" href="{{ route('users.manager-requests') }}">Show All Alerts</a>
+                                <a class="dropdown-item text-center small text-gray-500"
+                                    href="{{ route('users.manager-requests') }}">Show All Alerts</a>
                             </div>
                         </li>
 
@@ -161,12 +174,16 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
 
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->username }}</span>
-                                <img class="img-profile rounded-circle" src="{{ asset('images/other/profile-1.svg') }}">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span
+                                    class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->username }}</span>
+                                <img class="img-profile rounded-circle"
+                                    src="{{ asset('images/other/profile-1.svg') }}">
                             </a>
                             <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="{{ route('users.manager-requests') }}">
                                     <i class="fas fa-user-tie fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Manager Requests
@@ -176,7 +193,8 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
                                     Users Overview
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <a class="dropdown-item" href="#" data-toggle="modal"
+                                    data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
                                 </a>
@@ -221,7 +239,8 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
     </a>
 
     <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -235,7 +254,8 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
                     <form id="logout_form" method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <a class="btn btn-primary text-white" href="javascript:{}" onclick="document.getElementById('logout_form').submit();">Logout</a>
+                        <a class="btn btn-primary text-white" href="javascript:{}"
+                            onclick="document.getElementById('logout_form').submit();">Logout</a>
                     </form>
                 </div>
             </div>
@@ -251,6 +271,8 @@ $requests = \App\Models\User::where('wants_manager',1)->get();
 
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('js/sb-admin-2.js') }}"></script>
+
+    @stack('foot')
 
 </body>
 
